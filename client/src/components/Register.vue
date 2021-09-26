3b3bc32424456578e3b48e1356d09a97a0b714f1
Adn1217 <adn1217@hotmail.com>
2021-09-26 16:09:26 -0500

    Setting up the registration page

---
+++ client/src/components/Register.vue
@@ -0,0 +1,49 @@
+<template>
+<div>
+  <h1>Register</h1>
+  <input
+    type="email"
+    name="email"
+    v-model="email"
+    placeholder="email"/>
+  <br>
+  <input
+    type="password"
+    name="password"
+    v-model="password"
+    placeholder="password"/>
+  <br>
+  <button @click="register">Register</button>
+</div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService.js'
+export default {
+  data () {
+    return {
+      email: 'abc',
+      password: '123'
+    }
+  },
+  methods: {
+    async register () {
+      //  console.log('Register button was clicked', this.email, this.password)
+      const response = await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+      console.log(response.data)
+    }
+  },
+  watch: {
+    email (value) {
+      console.log('email has changed', value)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
